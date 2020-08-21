a6cf3cd6634f368847bc3e5a537c620949e8cba2
flamme <flamme1004@gmail.com>
2020-08-21 20:48:12 +0900

    immer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React,{ useRef, useReducer, useMemo, useCallback, createContext } from 'react';
+import produce from 'immer';
 import './App.css';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
@@ -9,6 +10,10 @@ import useInputs from './useInputs';
 //dispatch : 보내다
 
 //useReducer와 useState의 사용은 상황에 따라서 반복이 필요할때는 Reducer를 사용한다. 간단한거면 useState사용
+
+//yarn add immer
+//immer
+//immer를 사용하면 불변성을 해치는 코드를 작성해도 대신 불변설을 지켜준다.
 
 function countActiveUsers(users) {
   console.log('활성사용자 수를 세는중...')
@@ -40,24 +45,35 @@ const initialState = {
 function reducer(state, action) {
   switch(action.type) {
     case 'CREATE_USER' :
-      return {
-        inputs : initialState.inputs,
-        users : state.users.concat(action.user)
-      }
+      return produce(state, draft => {
+        draft.users.push(action.user);
+      });
+      // return {
+      //   inputs : initialState.inputs,
+      //   users : state.users.concat(action.user)
+      // }
     case 'TOGGLE_USER' : 
-      return {
-        ...state,
-        users : state.users.map(user => 
-          user.id === action.id 
-            ? {...user, active: !user.active}
-            : user
-        )
-      }
+      return produce(state, draft => {
+          const user = draft.users.find(user => user.id === action.id);
+          user.active = !user.active;
+      });
+      //return {
+      //  ...state,
+      //  users : state.users.map(user => 
+      //    user.id === action.id 
+      //      ? {...user, active: !user.active}
+      //      : user
+      //  )
+      //}
     case 'REMOVE_USER' : 
-      return {
-        ...state,
-        users: state.users.filter(user => user.id !== action.id)
-      }
+      return produce(state, draft => {
+        const index = draft.users.findIndex(user => user.id === action.id);
+        draft.users.splice(index,1);
+      });
+      //return {
+      //  ...state,
+      //  users: state.users.filter(user => user.id !== action.id)
+      //}
     default :
       throw new Error('Unhandled Acction');
   }
