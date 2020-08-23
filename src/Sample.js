dd8deb21dd41dfed3663f82e59a94fed0c5274f9
flamme <flamme1004@gmail.com>
2020-08-23 22:47:03 +0900

    snippet, eslint

---
+++ src/Sample.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ${TM_FILENAME_BASE}() {
+  return <div>Sample</div>;
+}
+
+export default ${TM_FILENAME_BASE};
+
+//Snippet 설정 --> code snippet에 저장 (snippet-generator.app)로 일단 만들고!
